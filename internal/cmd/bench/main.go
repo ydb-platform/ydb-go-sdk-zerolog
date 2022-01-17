2fcfedf8a0f4efd80fc83441a82796838e9c377e
asmyasnikov <79263256394@ya.ru>
2022-01-17 22:01:19 +0300

    fix bench

--- internal/cmd/bench/main.go
+++ internal/cmd/bench/main.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/rs/zerolog"
 
+	"github.com/ydb-platform/ydb-go-sdk/v3"
 	"github.com/ydb-platform/ydb-go-sdk/v3/balancer"
 	"github.com/ydb-platform/ydb-go-sdk/v3/table"
 	"github.com/ydb-platform/ydb-go-sdk/v3/table/options"
